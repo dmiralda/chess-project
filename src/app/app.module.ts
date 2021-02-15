936022d725ec3209f7ab1b513f2b14aee9dd428e
Daniel Miralda <danimirlo@gmail.com>
2021-02-15 21:21:54 +0100

    include allianz components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BoardViewerComponent } from './board-viewer/board-viewer.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NxAccordionModule } from '@aposin/ng-aquila/accordion';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BoardViewerComponent
-  ],
+  bootstrap: [AppComponent],
+  declarations: [AppComponent, BoardViewerComponent],
+  entryComponents: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NxAccordionModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
 export class AppModule { }
